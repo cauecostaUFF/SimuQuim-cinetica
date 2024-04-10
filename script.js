81dff8b074fa068804e1258c85b5fd8b59bbb82c
Cauê Costa <82660097+cauecostaUFF@users.noreply.github.com>
2024-04-10 12:59:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -68,8 +68,8 @@ class Ball{
     }
 
     changeVel(vel){
-        this.vel.x = this.orig_vel.x*(1+vel/10)
-        this.vel.y = this.orig_vel.y*(1+vel/10)
+        this.vel.x = this.orig_vel.x*(1+vel/25)
+        this.vel.y = this.orig_vel.y*(1+vel/25)
     }
 
 
@@ -78,6 +78,7 @@ class Ball{
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const ballRadius = 10;
+let piston = 0;
 
 
 let cont = 0;
@@ -85,8 +86,68 @@ let x = ballRadius;
 let y = canvas.height - ballRadius;
 let B = [];
 
+let vel = 0;
+let scroll = document.getElementById("scroll")
+document.getElementById("decreaseP").disabled = true
+
+document.getElementById("newBallA").addEventListener("click", function () {
+    newBall("green");
+});
+document.getElementById("newBallB").addEventListener("click", function () {
+    newBall("blue");
+});
+document.getElementById("reset").addEventListener("click", function () {
+    B = []; cont = 0; vel = 0;piston = 0;
+
+    document.getElementById("increaseT").disabled = false;
+    document.getElementById("decreaseT").disabled = false;
+    document.getElementById("increaseP").disabled = false;
+    document.getElementById("decreaseP").disabled = true;
+});
+document.getElementById("increaseT").addEventListener("click", function () {
+    vel += 5; document.getElementById("decreaseT").disabled = false;
+    if (vel<25){
+       for (let i = 0;i<B.length;i++){
+            B[i].changeVel(vel);
+        }
+    }else {
+        document.getElementById("increaseT").disabled = true;
+    }
+});
+document.getElementById("decreaseT").addEventListener("click", function () {
+    vel -= 5; document.getElementById("increaseT").disabled = false;
+    if (vel>-20){
+            for (let i = 0;i<B.length;i++){
+            B[i].changeVel(vel);
+        }
+    } else {
+        document.getElementById("decreaseT").disabled = true;
+    }
+});
+document.getElementById("increaseP").addEventListener("click", function () {
+    document.getElementById("decreaseP").disabled = false;
+    piston += 50;
+    if (piston>=450){
+        document.getElementById("increaseP").disabled = true;
+    }
+    for (let i = 0;i<B.length;i++){
+        
+    }
+});
+
+document.getElementById("decreaseP").addEventListener("click", function () {
+    document.getElementById("increaseP").disabled = false;
+    piston -= 50;
+    if (piston<=0){
+        document.getElementById("decreaseP").disabled = true;
+    }
+});
+
+const interval = setInterval(draw, 10);
+
+
 function newBall(col){
-    B[cont] = new Ball(x,y,ballRadius,(Math.random()*10 - 5)*(1+vel/10) ,(Math.random()*10 - 5)*(1+vel/10),col);
+    B[cont] = new Ball(x,y,ballRadius,(Math.random()*10 - 5)*(1+vel/25) ,(Math.random()*10 - 5)*(1+vel/25),col);
     cont +=1;
 }
 
@@ -94,18 +155,22 @@ function newBall(col){
 function draw() {
     
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    let p = ctx.rect(0,piston,1000,5);
+    ctx.fillStyle = "black";
+    ctx.fill();
+    ctx.closePath();
+
+
     for (let i = 0; i<B.length; i++){
-        
 
         if (B[i].pos.x + B[i].vel.x > canvas.width - ballRadius || B[i].pos.x + B[i].vel.x  < ballRadius) {
             B[i].vel.x*=-1 ;B[i].orig_vel.x*=-1;
         }
         
-        if (B[i].pos.y + B[i].vel.y > canvas.height - ballRadius || B[i].pos.y + B[i].vel.y  < ballRadius) {
+        if (B[i].pos.y + B[i].vel.y > canvas.height - ballRadius || B[i].pos.y + B[i].vel.y  < ballRadius+piston) {
             B[i].vel.y*=-1 ;B[i].orig_vel.y*=-1;
         }
-
-    
 
         B[i].pos.x += B[i].vel.x;
         B[i].pos.y += B[i].vel.y;
@@ -123,41 +188,11 @@ function draw() {
 
 }
 
-let vel = 0;
-document.getElementById("newBallA").addEventListener("click", function () {
-    newBall("green");
-});
-document.getElementById("newBallB").addEventListener("click", function () {
-    newBall("blue");
-});
-document.getElementById("reset").addEventListener("click", function () {
-    B = []; cont = 0; vel = 0; 
-});
-document.getElementById("increaseT").addEventListener("click", function () {
-    vel += 1; document.getElementById("decreaseT").disabled = false;
-    if (vel<=5){
-       for (let i = 0;i<B.length;i++){
-            B[i].changeVel(vel);
-        }
-    }else {
-        document.getElementById("increaseT").disabled = true;
-    }
-});
-document.getElementById("decreaseT").addEventListener("click", function () {
-    vel -= 1; document.getElementById("increaseT").disabled = false;
-    if (vel>=-5){
-            for (let i = 0;i<B.length;i++){
-            B[i].changeVel(vel);
-        }
-    } else {
-        document.getElementById("decreaseT").disabled = true;
-    }
-});
-const interval = setInterval(draw, 10);
+
 
 function coll_det_bb(b1, b2){
     if(b1.r + b2.r >= b2.pos.subtr(b1.pos).mag()){
-        if (Math.random()>0.9-(vel/10) && ((b1.color=="blue" && b2.color=="green")|| (b1.color=="green" && b2.color=="blue"))){
+        if (Math.random()>0.95-(vel/50) && ((b1.color=="blue" && b2.color=="green")|| (b1.color=="green" && b2.color=="blue"))){
             b1.status = false;
             b1.r = 0;
             b2.color = "red";
@@ -170,8 +205,6 @@ function coll_det_bb(b1, b2){
     }
 }
 
-//penetration resolution
-//repositions the balls based on the penetration depth and the collision normal
 function pen_res_bb(b1, b2){
     let dist = b1.pos.subtr(b2.pos);
     let pen_depth = b1.r + b2.r - dist.mag();
@@ -179,13 +212,3 @@ function pen_res_bb(b1, b2){
     b1.pos = b1.pos.add(pen_res);
     b2.pos = b2.pos.add(pen_res.mult(-1));
 }
-let scroll = document.getElementById("scroll")
-document.getElementById("increaseP").addEventListener("click", function () {
-    canvas.height -= 50;
-    scroll.height +=50;
-});
-
-document.getElementById("decreaseP").addEventListener("click", function () {
-    canvas.height += 50;
-    scroll.height -=50;
-});
